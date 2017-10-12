93365cbaefb48b97c9866a5a8475eca8a8f64558
Max Lebedev <maxlebedev@live.com>
2017-10-12 09:28:28 -0400

    improved comment, added more params

--- mulligan.py
+++ mulligan.py
@@ -1,18 +1,24 @@
+#!/usr/bin/python
 from __future__ import division
 import math
 import sys
 
 def fct(n):
 	return math.factorial(n)
+
 def c(n,d):
 	return fct(n)/(fct(d)*fct(n-d))
 
-def mullTo(x,h=0):#mulligan to a x-of but keep any h card hand
+def mullTo(x,h=0, crds=60, d=7):
+    """ x represents the number of desired cards in the deck, one of which constitutes a keep
+        h is the minimum hand size (h=4 means keep any 4 card hand)
+        crds is the number of cards in the deck at the start
+        d is the number of cards to draw initially
+    """
 	cumSum = 1;
-	d = 7;
 	while (d >= h):
-		cumSum = cumSum * c(60-x,d)/c(60,d)
+		cumSum = cumSum * c(crds-x,d)/c(crds,d)
 		d = d-1
-	return 1-cumSum;
+	return 1-cumSum
 
 print(mullTo(*map(int,sys.argv[1:])))
