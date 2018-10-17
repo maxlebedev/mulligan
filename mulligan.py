b76d12243291d56837d65aa4e24d35b10742ed87
Max Lebedev <maxlebedev@live.com>
2018-10-17 19:59:57 -0400

    fixed a typo, made a name longer

--- mulligan.py
+++ mulligan.py
@@ -9,17 +9,17 @@ def fct(n):
 def c(n,d):
     return fct(n)/(fct(d)*fct(n-d))
 
-def mull_to(pop, min_hand=0, deck_size=60, hand_size=7):
+def mull_to(pop, min_hand_size=0, deck_size=60, hand_size=7):
     """ pop represents the number of desired cards in the deck, one of which constitutes a keep
-        min_hand is the minimum hand size (min_hand=4 means keep any 4 card hand)
+        min_hand_size is the minimum hand size (min_hand_size=4 means keep any 4 card hand)
         deck_size is the number of cards in the deck at the start
         hand_size is the number of cards to draw initially
     """
-    chance_of_failue = 1;
-    while (hand_size >= min_hand):
+    chance_of_failure = 1;
+    while (hand_size >= min_hand_size):
             chance_of_failue *= c(deck_size - pop, hand_size)/c(deck_size, hand_size)
-            hand_size = hand_size-1
-    return 1 - chance_of_failue
+            hand_size -= 1
+    return 1 - chance_of_failure
 
 if __name__ == "__main__":
     print(mull_to(*map(int,sys.argv[1:])))
